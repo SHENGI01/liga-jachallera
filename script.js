f2c7433039f0c1fe3106ec5821bc822c9b16fa86
SHENGI01 <matiperez248@gmail.com>
2026-05-13 11:49:48 -0300

    Refactor partidos and equipos data structure

--- script.js
+++ script.js
@@ -25,12 +25,12 @@ function obtenerEquipo(nombre) {
 // =====================
 const fechas = {
   fecha1: [
-    { local: "Pampa", visitante: "Pampa Vieja", golesL:1, golesV:0 , cancha: "racing", hora: "14:00", estado: "proximo" },
-    { local: "San Lorenzo", visitante: "Andacollo", golesL: 0, golesV: 0, cancha: "niquivil", hora: "14:00", estado: "proximo" },
-    { local: "Central Norte", visitante: "Estrella", golesL: 0, golesV: 0, cancha: "arbol verde", hora: "14:00",estado: "proximo" },
-    { local: "Peñarol", visitante: "Arbol Verde", golesL: 0, golesV: 0, cancha: "arbol verde", hora: "16:00", estado: "proximo" },
-    { local: "Niquivil", visitante: "Florida", golesL: 0, golesV: 0, cancha: "niquivil", hora: "16:00", estado: "proximo" },
-    { local: "El Rincon", visitante: "Racing", golesL: 0, golesV: 0, cancha: "racing", hora: "16:00",estado: "proximo" }
+    { local: "Pampa", visitante: "Pampa Vieja", golesL:1, golesV:0 , cancha: "Racing", hora: "14:00", estado: "proximo" },
+    { local: "San Lorenzo", visitante: "Andacollo", golesL: 0, golesV: 0, cancha: "Niquivil", hora: "14:00", estado: "proximo" },
+    { local: "Central Norte", visitante: "Estrella", golesL: 0, golesV: 0, cancha: "Arbol verde", hora: "14:00",estado: "proximo" },
+    { local: "Peñarol", visitante: "Arbol Verde", golesL: 0, golesV: 0, cancha: "Arbol verde", hora: "16:00", estado: "proximo" },
+    { local: "Niquivil", visitante: "Florida", golesL: 0, golesV: 0, cancha: "Niquivil", hora: "16:00", estado: "proximo" },
+    { local: "El Rincon", visitante: "Racing", golesL: 0, golesV: 0, cancha: "Racing", hora: "16:00",estado: "proximo" }
   ],
 
   fecha2: [
