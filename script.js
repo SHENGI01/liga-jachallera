ba996afb996939ceb6e272c417bbd17b2e486f93
SHENGI01 <matiperez248@gmail.com>
2026-05-21 11:47:03 -0300

    Refactor category data structure and update logic

--- script.js
+++ script.js
@@ -60,9 +60,9 @@ const categorias = {
     fechas: {
 
       fecha1: [
-        { local: "La Frontera", visitante: "Independiente", cancha: "pampa", hora: "17:00", estado: "proximo" },
+        { local: "La Frontera", visitante: "Independiente", cancha: "pampa", hora: "16:00", estado: "proximo" },
         { local: "San Martin", visitante: "Boca del Medano",  cancha: "pampa", hora: "14:00", estado: "proximo" },
-        { local: "La Falda", visitante: "Otra Banda",cancha: "la falda", hora: "17:00", estado: "proximo" },
+        { local: "La Falda", visitante: "Otra Banda",cancha: "la falda", hora: "16:00", estado: "proximo" },
         { local: "San Blas", visitante: "San Roque", cancha: "la falda", hora: "14:00", estado: "proximo" },
         { local: "Calle Varas", visitante: "Huaco",cancha: "libre", hora: "libre", estado: "libres" }
       ],
