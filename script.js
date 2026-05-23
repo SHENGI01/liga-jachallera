28125b7bcfd08b0487ddad3ae49c4af650457db3
SHENGI01 <matiperez248@gmail.com>
2026-05-23 16:18:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -60,9 +60,9 @@ const categorias = {
     fechas: {
 
       fecha1: [
-        { local: "La Frontera", visitante: "Independiente",cancha: "pampa", hora: "16:30", estado: "proximo" },
+        { local: "La Frontera", visitante: "Independiente",cancha: "pampa",golesL: 0, golesV: 0, hora: "16:30", estado: "vivo" },
         { local: "San Martin", visitante: "Boca del Medano", golesL: 2, golesV: 1, cancha: "pampa", hora: "14:00", estado: "finalizado" },
-        { local: "La Falda", visitante: "Otra Banda",cancha: "la falda", hora: "16:30", estado: "proximo" },
+        { local: "La Falda", visitante: "Otra Banda",golesL: 0, golesV: 0,cancha: "la falda", hora: "16:30", estado: "vivo" },
         { local: "San Blas", visitante: "San Roque",golesL: 0, golesV: 0, cancha: "la falda", hora: "14:00", estado: "finalizado" },
         { local: "Calle Varas", visitante: "Huaco",cancha: "libre", hora: "libre", estado: "libres" }
       ],
