a29ccff2edb7dac96782a2e113dde08b5551cbf0
SHENGI01 <matiperez248@gmail.com>
2026-05-24 15:33:37 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const categorias = {
       fecha1: [
         { local: "Pampa", visitante: "Pampa Vieja",golesL: 1, golesV: 0, cancha: "racing", hora: "14:00", estado: "vivo" },
         { local: "San Lorenzo", visitante: "Andacollo",golesL: 0, golesV: 0,cancha: "niquivil", hora: "14:00", estado: "vivo" },
-        { local: "Central Norte", visitante: "Estrella",golesL: 0, golesV: 0, cancha: "arbol verde", hora: "14:00", estado: "vivo" },
+        { local: "Central Norte", visitante: "Estrella",golesL: 1, golesV: 0, cancha: "arbol verde", hora: "14:00", estado: "vivo" },
         { local: "Peñarol", visitante: "Arbol Verde",cancha: "arbol verde", hora: "16:30", estado: "proximo" },
         { local: "Niquivil", visitante: "Florida",cancha: "niquivil", hora: "16:30", estado: "proximo" },
         { local: "El Rincon", visitante: "Racing", cancha: "racing", hora: "16:30", estado: "proximo" }
