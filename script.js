99494b232d289eb9b9c7b4437000735f4d89f3c9
shengistd <matiperez248@gmail.com>
2026-05-30 16:09:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ const categorias = {
  fecha2: [
         { local: "San Martin", visitante: "Otra Banda",golesL: 0, golesV: 4,cancha: "huaco",hora: "14:00", estado: "vivo" },
         { local: "Huaco", visitante: "San Blas", cancha: "huaco", hora: "16:30", estado: "proximo" },
-        { local: "Calle Varas", visitante: "La Frontera",golesL: 2, golesV: 3,cancha: "hungaros", hora: "14:00", estado: "vivo" },
+        { local: "Calle Varas", visitante: "La Frontera",golesL: 2, golesV: 3,cancha: "hungaros", hora: "14:00", estado: "finalizado" },
         { local: "Independiente", visitante: "La Falda",cancha: "hungaros", hora: "16:30", estado: "proximo" },
         { local: "San Roque", visitante: "Boca Del Medano",cancha: "libre", hora: "libre", estado: "libres" }
       ],
@@ -101,7 +101,8 @@ const categorias = {
       { nombre: "Emanuel Cabanay", goles: 1 },
       { nombre: "Bautista Carrizo", goles: 1 },
       { nombre: "Facundo Balmaceda", goles: 1 },
-      { nombre: "Elias Aciar", goles: 1 },
+      { nombre: "Elias Aciar", goles: 1 },
+
       { nombre: "  Emiliano Diaz", goles: 1 }
     ]
 
