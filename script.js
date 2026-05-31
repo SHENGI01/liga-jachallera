8e29436eb9ed2640f3154dc2fb783513d8e81d01
shengistd <matiperez248@gmail.com>
2026-05-31 14:10:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,10 +33,10 @@ const categorias = {
       ],
     fecha2: [
         { local: "Pampa", visitante: "Andacollo",cancha: "pampa", hora: "16:30", estado: "proximo" },
-        { local: "Peñarol", visitante: "Racing",cancha: "pampa", hora: "14:00", estado: "proximo" },
-        { local: "San Lorenzo", visitante: "Niquivil",cancha: "florida", hora: "14:00", estado: "proximo" },
+        { local: "Peñarol", visitante: "Racing",golesL: 0, golesV: 0,cancha: "pampa", hora: "14:00", estado: "vivo" },
+        { local: "San Lorenzo", visitante: "Niquivil",golesL: 0, golesV: 0,cancha: "florida", hora: "14:00", estado: "vivo" },
         { local: "Florida", visitante: "Arbol Verde",cancha: "florida", hora: "16:30", estado: "proximo" },
-        { local: "Pampa Vieja", visitante: "Central Norte",cancha: "estrella", hora: "14:00", estado: "proximo" },
+        { local: "Pampa Vieja", visitante: "Central Norte",golesL: 0, golesV: 0,cancha: "estrella", hora: "14:00", estado: "vivo" },
         { local: "El Rincon", visitante: "Estrella",cancha: "estrella", hora: "16:30", estado: "proximo" }
       ]
 
