5f02de4f7318f52d2fbd327050c4cd6a767c91c3
shengistd <matiperez248@gmail.com>
2026-06-06 16:11:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ const categorias = {
         { local: "San Roque", visitante: "Boca Del Medano",cancha: "libre", hora: "libre", estado: "libres" }
       ],
          fecha3: [
-        { local: "La Frontera", visitante: "Boca Del Medano",golesL: 7, golesV: 1, cancha: "san martin", hora: "14:00", estado: "vivo" },
+        { local: "La Frontera", visitante: "Boca Del Medano",golesL: 7, golesV: 1, cancha: "san martin", hora: "14:00", estado: "finalizado" },
         { local: "San Martin", visitante: "San Roque", cancha: "san martin", hora: "16:30", estado: "proximo" },
         { local: "Huaco", visitante: "La Falda",golesL: 1, golesV: 0, cancha: "peñarol", hora: "14:00", estado: "finalizado" },
         { local: "Independiente", visitante: "Calle Varas", cancha: "peñarol", hora: "16:30", estado: "proximo" },
