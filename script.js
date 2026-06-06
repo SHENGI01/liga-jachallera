0349c84788a1dc6b46eb3d845a31de6de83e256e
shengistd <matiperez248@gmail.com>
2026-06-06 16:55:45 -0300

    Update fmt.Println output from 'Hello World' to 'Goodbye World'

--- script.js
+++ script.js
@@ -181,9 +181,9 @@ const categorias = {
         { local: "San Roque", visitante: "Boca Del Medano",cancha: "libre", hora: "libre", estado: "libres" }
       ],
          fecha3: [
-        { local: "La Frontera", visitante: "Boca Del Medano",golesL: 7, golesV: 1,cancha: "san martin", hora: "14:00", estado: "finalizado" },
         { local: "San Martin", visitante: "San Roque",golesL: 0, golesV: 0, cancha: "san martin", hora: "16:30", estado: "vivo" },
         { local: "Huaco", visitante: "La Falda",golesL: 1, golesV: 0, cancha: "peñarol", hora: "14:00", estado: "finalizado" },
+        { local: "La Frontera", visitante: "Boca Del Medano",golesL: 7, golesV: 1, cancha: "san martin", hora: "14:00", estado: "finalizado" },
         { local: "Independiente", visitante: "Calle Varas", golesL: 0, golesV: 0,cancha: "peñarol", hora: "16:30", estado: "vivo" },
         { local: "San Blas", visitante: "Otra Banda", cancha: "libre", hora: "libre", estado: "libres" }
       ]
