8abdf5401030c8d47ec2229bfd944760129cc230
shengistd <matiperez248@gmail.com>
2026-06-07 15:57:53 -0300

    Update add function to accept parameters

--- script.js
+++ script.js
@@ -43,9 +43,9 @@ const categorias = {
         { local: "San Lorenzo", visitante: "Florida",golesL: 0, golesV: 0,cancha:"san lorenzo", hora: "16:30", estado: "proximo" },
         { local: "Peñarol", visitante: "Estrella",golesL: 0, golesV: 0,cancha: "peñarol", hora: "16:30", estado: "proximo" },
         { local: "Central Norte", visitante: "Andacollo",golesL: 0, golesV: 0, cancha: "andacollo", hora: "16:30", estado: "proximo" },
-        { local: "Racing", visitante: "Arbol Verde",golesL: 2, golesV: 3, cancha:"andacollo", hora: "14:00", estado: "vivo" },
-        { local: "Pampa Vieja", visitante: "El Rincon",golesL: 1, golesV: 1, cancha: "san lorenzo", hora: "14:00", estado: "vivo" },
-        { local: "Pampa", visitante: "Niquivil",golesL: 1, golesV: 2, cancha:"peñarol", hora: "14:00", estado: "vivo" }
+        { local: "Racing", visitante: "Arbol Verde",golesL: 2, golesV: 3, cancha:"andacollo", hora: "14:00", estado: "finalizado" },
+        { local: "Pampa Vieja", visitante: "El Rincon",golesL: 1, golesV: 1, cancha: "san lorenzo", hora: "14:00", estado: "finalizado" },
+        { local: "Pampa", visitante: "Niquivil",golesL: 1, golesV: 2, cancha:"peñarol", hora: "14:00", estado: "finalizado" }
       ],
       fecha4: [
         { local: "Pampa", visitante: "Florida",cancha:"sin definir", hora: "sin definir", estado: "proximo" },
