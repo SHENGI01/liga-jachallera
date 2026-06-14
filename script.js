daf69de19b9f5b67509c2609eff5cc0c5aed125a
shengistd <matiperez248@gmail.com>
2026-06-14 14:19:09 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ const categorias = {
         { local: "EL Rincon", visitante: "Andacollo",cancha: "san lorenzo", hora: "16:30", estado: "proximo" },
         { local: "San Lorenzo", visitante: "Arbol Verde",golesL: 0, golesV: 0,cancha: "pampa", hora: "14:00", estado: "vivo" },
         { local: "Estrella", visitante: "Racing",golesL: 0, golesV: 0,cancha: "san lorenzo", hora: "14:00", estado: "vivo" },
-        { local: "Central Norte", visitante: "Niquivil",golesL: 0, golesV: 0,cancha: "pampa vieja", hora: "14:00", estado: "vivo" }
+        { local: "Central Norte", visitante: "Niquivil",golesL: 0, golesV: 1,cancha: "pampa vieja", hora: "14:00", estado: "vivo" }
       ],
       fecha5: [
         { local: "Arbol Verde", visitante: "Estrella",cancha:"sin definir", hora: "sin definir", estado: "proximo" },
