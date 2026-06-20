128f94adf0e3df2d0e39b97b2524216c66031d72
shengistd <matiperez248@gmail.com>
2026-06-20 17:49:48 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ const categorias = {
         { local: "Calle Varas", visitante: "San Roque",golesL: 3, golesV: 3,cancha: "huaco", hora: "14:00", estado: "finalizado" },
         { local: "Otra Banda", visitante: "Independiente",golesL: 0, golesV: 1,cancha: "hungaros", hora: "16:30", estado: "vivo" },
         { local: "San Martin", visitante: "San Blas",golesL: 1, golesV: 2,cancha: "hungaros", hora: "14:00", estado: "finalizado" },
-        { local: "Huaco", visitante: "La Frontera",golesL: 0, golesV: 0,cancha: " huaco", hora: "16:30", estado: "vivo" },
+        { local: "Huaco", visitante: "La Frontera",golesL: 0, golesV: 1,cancha: " huaco", hora: "16:30", estado: "vivo" },
         { local: "La Falda", visitante: "Boca del Medano", cancha: "libre", hora: "libre", estado: "libres" } 
       ],
     },
