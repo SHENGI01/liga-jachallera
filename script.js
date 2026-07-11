08e27902f93ef1e5d77e27f194550b3c35e03749
shengistd <matiperez248@gmail.com>
2026-07-11 18:31:04 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -229,7 +229,7 @@ const categorias = {
         fecha7: [
         { local: "Independiente", visitante: "San Roque",golesL: 4, golesV: 0,cancha: "pampa", hora: "14:00", estado: "finalizado" },
         { local: "San Martin", visitante: "La Frontera",cancha: "pampa", hora: "16:30", estado: "vivo" },
-        { local: "La Falda", visitante: "Boca del Medano",golesL: 4, golesV: 0,cancha: "la falda", hora: "14:00", estado: "finalizado" },
+        { local: "La Falda", visitante: "Boca del Medano",golesL: 2, golesV: 2,cancha: "la falda", hora: "14:00", estado: "finalizado" },
         { local: "San Blas", visitante: "Otra Banda",golesL: 0, golesV: 0,cancha: " la falda", hora: "16:30", estado: "vivo" },
         { local: "Calle Varas", visitante: "Huaco", golesL: 0, golesV: 0,cancha: "libre", hora: "libre", estado: "libres" } 
       ],
