0aea9f262161eed665d4488df1ef129bed6c8ab1
shengistd <matiperez248@gmail.com>
2026-07-18 15:44:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -235,9 +235,9 @@ const categorias = {
         { local: "Calle Varas", visitante: "Huaco",cancha: "libre", hora: "libre", estado: "libres" } 
       ],
        fecha8: [
-        { local: "Otra Banda", visitante: "San Roque",cancha: "la falda", hora: "14:00", estado: "proximo" },
+        { local: "Otra Banda", visitante: "San Roque",golesL: 0, golesV: 0,cancha: "la falda", hora: "14:00", estado: "vivo" },
         { local: "La Falda", visitante: "La Frontera",cancha: "la falda", hora: "16:30", estado: "proximo" },
-        { local: "Huaco", visitante: "Boca del Medano",cancha: "peñarol", hora: "14:00", estado: "proximo" },
+        { local: "Huaco", visitante: "Boca del Medano",golesL: 0, golesV: 0,cancha: "peñarol", hora: "14:00", estado: "vivo" },
         { local: "San Blas", visitante: "Calle Varas",cancha: " peñarol", hora: "16:30", estado: "proximo" },
         { local: "San Martin", visitante: "Independiente",cancha: "libre", hora: "libre", estado: "libres" } 
       ],
